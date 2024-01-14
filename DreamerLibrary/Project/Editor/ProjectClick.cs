7095577886350eef24615d5215613f5a9dd73f19
DESKTOP-OS6P9I2\78503 <785033237@qq.com>
2024-01-14 20:46:18 +0800

    Scenes切换

--- DreamerLibrary/Project/Editor/ProjectClick.cs
+++ DreamerLibrary/Project/Editor/ProjectClick.cs
@@ -10,4 +10,18 @@ public class ProjectClick
     {
         Debug.LogError("Project Test");
     }
+
+    //Unity编译完成就去调用这个方法
+    [InitializeOnLoadMethod]
+    //方法必须是静态的才能被调用
+    static void InitProject()
+    {
+        //这个方法用了委托
+        //
+        EditorApplication.projectWindowItemOnGUI = (string guid, Rect selectionRect) =>
+        {
+            Debug.LogError(1);
+        };
+        
+    }
 }
---
+++ DreamerLibrary/Scene/Editor/ScenesChange.cs
@@ -0,0 +1,42 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class ScenesChange : Editor
+{
+    [InitializeOnLoadMethod]
+    static void InitSwitcher()
+    {
+        SceneView.duringSceneGui += (SceneView scene) =>
+        {
+            Handles.BeginGUI();
+            {
+                EditorGUILayout.LabelField("Scenes«–ªª£∫");
+                if (GUILayout.Button("SampleScene", GUILayout.Width(100)))
+                {
+                    //var SampleScene = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Scenes/SampleScene.unity");
+                    //SceneManager.LoadScene("SampleScene");
+                    //GraphicsSettings.defaultRenderPipeline = pipline;
+                    EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity");
+
+                }
+                if (GUILayout.Button("Scenes", GUILayout.Width(100)))
+                {
+                    //var Scenes = AssetDatabase.LoadAssetAtPath<RenderPipelineAsset>("Assets/Scenes/Scenes.unity");
+                    //GraphicsSettings.defaultRenderPipeline = pipline;
+                    //SceneManager.LoadScene("Scenes");
+                    //AssetDatabase.OpenAsset(Scenes);
+                    EditorSceneManager.OpenScene("Assets/Scenes/Scenes.unity");
+                }
+            }
+            Handles.EndGUI();
+        };
+
+    }
+}
