012dd290ec49c954724ab3fe3e1f86ec5b600da6
DESKTOP-OS6P9I2\78503 <785033237@qq.com>
2024-02-13 21:43:12 +0800

    ModelPostProcessor

--- DreamerLibrary/AssetPostProcessor/Editor/ModelProcessor.cs
+++ DreamerLibrary/AssetPostProcessor/Editor/ModelProcessor.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class ModelProcessor : MonoBehaviour
+public class ModelProcessor : AssetPostprocessor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
