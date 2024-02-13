24ca161fc60920d6cdede1f814f1a99bb2e93857
DESKTOP-OS6P9I2\78503 <785033237@qq.com>
2024-02-13 22:09:53 +0800

    ModelProcess

--- DreamerLibrary/AssetPostProcessor/Editor/ModelProcessor.cs
+++ DreamerLibrary/AssetPostProcessor/Editor/ModelProcessor.cs
@@ -5,5 +5,15 @@ using UnityEditor;
 
 public class ModelProcessor : AssetPostprocessor
 {
-    
+    //创建一个资源或者导入一个资源这个方法就会执行（OnPreprocessAsset()）
+    //当任何类型的资源被导入时执行（OnPreprocessAsset()）
+    void OnPreprocessAsset()
+    {
+        Debug.LogError("OnPreprocessAsset");
+    }
+    //当导入模型时执行(OnPostprocessModel())
+    void OnPostprocessModel()
+    {
+        Debug.LogError("OnPostprocessModel()");
+    }
 }
